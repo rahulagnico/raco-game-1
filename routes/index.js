bc076c3859281bde6045503c7145a6c82b7f2c53
Rahul Agnihotri <lifevitae@gmail.com>
2022-03-18 17:59:43 +0530

    added jokes package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const Joke = require('awesome-dev-jokes');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dada...' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'RACo Game 1', joke: Joke.getRandomJoke() });
 });
 
 module.exports = router;
