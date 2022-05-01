75f31537e5bf31f6277c6864b7466e84409917ac
Rahul Agnihotri <lifevitae@gmail.com>
2022-05-01 20:27:57 +0530

    added new jokes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
-const Joke = require('awesome-dev-jokes');
 var router = express.Router();
+var arr = require('../data/jokes.json');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'RACo Game 1', joke: Joke.getRandomJoke() });
+  res.render('index', { title: 'RACo Game 1', joke: getRandomJoke() });
 });
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var getRandomJoke = function () {
+    var i = randomNumber(0, arr.length - 1);
+    return arr[i].joke;
+};
 
 module.exports = router;
